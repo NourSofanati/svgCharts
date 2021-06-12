ea1ce3478e4c7332988073bba5a0c51b2e15d264
= <=>
2021-06-12 15:13:17 +0300

    Finished Pie Chart SVG ✅

--- js/main.js
+++ js/main.js
@@ -1,1 +1,10 @@
-"use strict";
+import Pie from "./classes/Pie.js";
+const container = document.getElementById('pie-chart');
+drawChart(container);
+function drawChart(div) {
+    let pie = new Pie("pie-chart");
+    pie.AddSlice(500, "كهرباء");
+    pie.AddSlice(500, "ماء");
+    pie.AddSlice(2500, "انترنت");
+    pie.AddSlice(55000, "طعام");
+}
